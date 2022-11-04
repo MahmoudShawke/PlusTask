41017bc6ee769332b1aa5681f80d4e3384b91b68
Mahmoud Shawky <shawke1235@yahoo.com>
2022-11-04 18:17:28 +0200

    Add all

---
+++ Custom/Task2/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Task2",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Shawky",
+    'category': 'Category',
+    'description': """
+     Task1
+    """,
+    'data': [
+        'sdasd'
+    ],
+
+}
